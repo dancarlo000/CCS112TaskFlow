edb688c836b254795aef2e9dd94ca0a45bc720c9
Junver Dairo <kingjunver@gmail.com>
2025-10-12 11:45:15 +0800

    Task 3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from "react"; 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
-import Header from "./components/Header"; 
-import AddTaskView from "./components/AddTaskView"; 
-import TaskListView from "./components/TaskListView"; 
- 
-function App() { 
-  return ( 
-    <Router> 
-      <Header /> 
-      <Routes> 
-        <Route path="/" element={<TaskListView />} /> 
-        <Route path="/add" element={<AddTaskView />} /> 
-      </Routes> 
-    </Router> 
-  ); 
-} 
- 
-export default App; 
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import AddTaskView from "./components/AddTaskView";
+import TaskListView from "./components/TaskListView";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (newTask) => setTasks([...tasks, newTask]);
+  const deleteTask = (index) => setTasks(tasks.filter((_, i) => i !== index));
+
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<TaskListView tasks={tasks} deleteTask={deleteTask} />} />
+        <Route path="/add" element={<AddTaskView addTask={addTask} />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
